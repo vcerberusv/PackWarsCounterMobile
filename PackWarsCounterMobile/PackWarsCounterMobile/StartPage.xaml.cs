173f187e5faa0051d3f5798504d51cbeeb15995c
vcerberusv <j.a.barnes1911@gmail.com>
2016-05-25 16:14:18 +1000

    First commit for Pack Wars Counter Mobile

---
+++ PackWarsCounterMobile/PackWarsCounterMobile/StartPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PackWarsCounterMobile
+{
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        void ToChooseGame(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new GameChoices());
+        }
+    }
+}
