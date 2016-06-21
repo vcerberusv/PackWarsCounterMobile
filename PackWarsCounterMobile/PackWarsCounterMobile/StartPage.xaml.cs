b0872f85e04038d754d6656b11b0f6c059d9a4c5
vcerberusv <j.a.barnes1911@gmail.com>
2016-06-21 17:33:37 +1000

    Added a description to the start page of the app

--- PackWarsCounterMobile/PackWarsCounterMobile/StartPage.xaml.cs
+++ PackWarsCounterMobile/PackWarsCounterMobile/StartPage.xaml.cs
@@ -9,6 +9,10 @@ namespace PackWarsCounterMobile
         public StartPage()
         {
             InitializeComponent();
+            welcomeLabel.Text = "Welcome to the Pack Wars Counter!";
+            chooseButton.Text = "Choose Your Game!!!";
+            descriptionPW.Text = "Pack Wars is game to be played among friends where each player has a certain number of card packs of the same game and depending on the cards recieved, gets points depending on the rarity of the card.";
+            descriptionPWC.Text = "With this counter, you choose the game you wish to play and and how many packs you are opening then it will give you a counter to add up your score of the cards you have opened.";
         }
 
         void ToChooseGame(object sender, EventArgs e)
